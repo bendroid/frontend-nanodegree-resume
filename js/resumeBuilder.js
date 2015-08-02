30ca702f6424328dab8e3d4c37090f22345f9c15
bendroid <collins.drums@gmail.com>
2015-08-02 17:32:02 -0400

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,14 +8,22 @@ var email = "collins.drums@gmail.com";
 var twitter = "@jaemood";
 var github = "bendroid";
 var mobile = "(555)-555-5555";
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+var bio = {
+	"name" : "Ben Collins",
+	"age" : 33,
+	"skills" : skills
+	};
 var myName = HTMLheaderName.replace("%data%", name);
 var myEmail = HTMLemail.replace("%data%", email);
 var myRole = HTMLheaderRole.replace("%data%", role);
 var myTwitter = HTMLtwitter.replace("%data%", twitter);
 var myGithub = HTMLgithub.replace("%data%", github);
 var myMobile = HTMLmobile.replace("%data%", mobile);
+
 $("#header").prepend(myRole);
 $("#header").prepend(myName);
+$("#main").append(bio.skills);
 $("#footerContacts").append(myEmail);
 $("#footerContacts").append(myTwitter);
 $("#footerContacts").append(myGithub);
