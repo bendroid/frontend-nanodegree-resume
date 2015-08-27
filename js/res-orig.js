4be233ee357643bb32467a055a54b738390ad5a1
Ben Collins <collins.drums@gmail.com>
2015-08-27 17:07:56 -0400

    update

--- js/res-orig.js
+++ js/res-orig.js
@@ -8,22 +8,29 @@ var email = "collins.drums@gmail.com";
 var twitter = "@jaemood";
 var github = "bendroid";
 var mobile = "(555)-555-5555";
-var skills = ["awesomeness" + "<br>", "programming" + "<br>", "teaching" + "<br>", "JS"];
-
+var skills = ["awesomeness", " programming", " teaching", " JS"];
 var locale = "Rochester, NY";
 var pic = "./images/fry.jpg";
-var welcome = "Welcome to my site!";
-var skillsstart = "Skills at a Glance:";
+
+
 var work = {};
 work.position = "Doing Work";
 work.employer = "My Job";
 work.years = 0.3;
-work.start = "2010";
 
 var education = {};
 education["name"] = "School of Hard Knocks";
 education["years"] = "2001-2005"
 education["city"] = "New York";
+
+$("main").append(work["position"]);
+$("main").append(education.name);
+
+
+var welcome = "Welcome to my site!";
+var skillsstart = "Skills at a Glance:";
+
+var workstart = "2010";
 
 var myPic = HTMLbioPic.replace("%data%", pic);
 var myName = HTMLheaderName.replace("%data%", name);
@@ -35,15 +42,18 @@ var myMobile = HTMLmobile.replace("%data%", mobile);
 var myLocation = HTMLlocation.replace("%data%", locale);
 var myWelcome = HTMLwelcomeMsg.replace("%data%", welcome);
 var mySkillsStart = HTMLskillsStart.replace("%data%", skillsstart);
-var skills = HTMLskills.replace("%data%", skills);
-//var mySkills = HTMLskills.replace("%data%", skills);
+var mySkills = HTMLskills.replace("%data%", skills);
+
+
 
 $("#header").prepend(myRole);
 $("#header").prepend(myName);
 $("#header").append(myWelcome);
 $("#header").append(myPic);
 $("#header").append(mySkillsStart);
-$("#header").append(skills);
+$("#header").append(mySkills);
+$("#main").append(bio.city);
+$("#workExperience").append(bio.workstart);
 $("#topContacts").append(myEmail);
 $("#topContacts").append(myTwitter);
 $("#topContacts").append(myGithub);
@@ -54,6 +64,3 @@ $("#footerContacts").append(myTwitter);
 $("#footerContacts").append(myGithub);
 $("#footerContacts").append(myMobile);
 $("#footerContacts").append(myLocation);
-$("main").append(work["position"]);
-$("main").append(work.start);
-$("main").append(education.name);
