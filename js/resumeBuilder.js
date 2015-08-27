dc306d906c300e2eb749321b7b208d1a9b04b01d
bendroid <collins.drums@gmail.com>
2015-08-27 11:53:56 -0400

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,15 +11,26 @@ var mobile = "(555)-555-5555";
 var skills = ["awesomeness", " programming", " teaching", " JS"];
 var locale = "Rochester, NY";
 var pic = "./images/fry.jpg";
-var bio = {
-	"name" : "Ben Collins",
-	"age" : 33,
-	"skills" : skills,
-	"pic" : pic
-	};
+
+
+var work = {};
+work.position = "Doing Work";
+work.employer = "My Job";
+work.years = 0.3;
+
+var education = {};
+education["name"] = "School of Hard Knocks";
+education["years"] = "2001-2005"
+education["city"] = "New York";
+
+$("main").append(work["position"]);
+$("main").append(education.name);
+
+
 var welcome = "Welcome to my site!";
 var skillsstart = "Skills at a Glance:";
 
+var workstart = "2010";
 
 var myPic = HTMLbioPic.replace("%data%", pic);
 var myName = HTMLheaderName.replace("%data%", name);
@@ -33,12 +44,62 @@ var myWelcome = HTMLwelcomeMsg.replace("%data%", welcome);
 var mySkillsStart = HTMLskillsStart.replace("%data%", skillsstart);
 var mySkills = HTMLskills.replace("%data%", skills);
 
+//
+
+var bio = {
+	"name" : "Ben Collins",
+	"role" : "Web Developer"
+	"contacts": {
+		"mobile": "(555)-555-5555",
+		"email": "email@myemail.com";
+		"github": "bendroid",
+		"twitter": "@jaemood",
+		"location": "Rochester, NY"
+	},
+	"welcomeMessage": "Welcome to my site!",
+	"skills": ["awesomeness", "programming", "teaching", "JS"],
+	"bioPic": "./images/fry.jpg"
+};
+//
+
+//
+var work = {};
+work.position = "Doing Work";
+work.employer = "My Job";
+work.years = 0.3;
+
+var education = {};
+education["name"] = "School of Hard Knocks";
+education["years"] = "2001-2005"
+education["city"] = "New York";
+
+$("main").append(work["position"]);
+$("main").append(education.name);
+//
+
+//
+if(bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	("#skills").append(formattedSkill);
+}
+//
+
 $("#header").prepend(myRole);
 $("#header").prepend(myName);
 $("#header").append(myWelcome);
 $("#header").append(myPic);
 $("#header").append(mySkillsStart);
 $("#header").append(mySkills);
+$("#main").append(bio.city);
+$("#workExperience").append(bio.workstart);
 $("#topContacts").append(myEmail);
 $("#topContacts").append(myTwitter);
 $("#topContacts").append(myGithub);
